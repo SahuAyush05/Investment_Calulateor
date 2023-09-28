53886f7c315dcd63c8fb69a9263a6b6089aac102
Ayush Sahu <ayushsahu050502@gmail.com>
2023-09-29 01:00:42 +0530

    npm install

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import logo from '../../assets/investment-calculator-logo.png';
+function Header(){
+    <header className="header">
+        <img src={logo} alt="logo" />
+        <h1>Investment Calculator</h1>
+      </header>
+}
+ 
+export default Header;
